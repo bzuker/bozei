6fcb1a4d5564801a43cd8323554a718ae12f21d4
Brian Zuker <brianzuker@gmail.com>
2020-10-19 21:37:27 -0300

    remove ensurelogin

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
-import EnsureLogin from "../components/EnsureLogin";
 import AuthProvider from "../context/Auth";
 import "../css/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <EnsureLogin>
-        <Component {...pageProps} />
-      </EnsureLogin>
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
