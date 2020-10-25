bbfd5c9d16fd8b61a6a679b632c824c9a5dbfc3a
Brian Zuker <brianzuker@gmail.com>
2020-10-25 11:14:09 -0300

    overflow for modal

--- components/Modal.js
+++ components/Modal.js
@@ -9,7 +9,7 @@ export default function Modal({ children, title, ...rest }) {
   return (
     <ReactModal
       {...rest}
-      className="max-h-screen max-w-3xl outline-none w-full"
+      className="max-h-screen max-w-3xl outline-none w-full overflow-auto"
       overlayClassName="items-center bg-gray-500 bottom-0 left-0 right-0 top-0 bg-opacity-50 fixed z-50 flex justify-center"
     >
       <div className="bg-white p-4 rounded shadow">
